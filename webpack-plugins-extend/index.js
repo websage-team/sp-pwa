34bfa5bd7c42c3ca900ef4d115b9a29c4cd04ca7
Diablohu <diablohudream@gmail.com>
2017-04-20 17:22:19 +0800

    minor fixes

--- webpack-plugins-extend/index.js
+++ webpack-plugins-extend/index.js
@@ -1,8 +1,8 @@
 const WebpackOnBuildPlugin = require('on-build-webpack')
-const create = require('../service-worker/index.js')
+const create = require('../create/index.js')
 
 module.exports = (outputPath) => {
-    new WebpackOnBuildPlugin(function (stats) {
+    return new WebpackOnBuildPlugin(function (stats) {
         // After webpack build...
         create(outputPath)
     })
