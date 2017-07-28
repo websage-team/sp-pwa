75f458c7a7392b4d22021bc77cbcc588d8790743
Diablohu <diablohu@users.noreply.github.com>
2017-07-29 03:43:01 +0800

    get-service-worker-file can now set distPath

--- get-service-worker-file/index.js
+++ get-service-worker-file/index.js
@@ -1,3 +1,3 @@
 import { getFile } from 'sp-isomorphic-utils'
 
-export default (filename = 'service-worker.js') => getFile(filename)
+export default (filename = 'service-worker.js', distPath) => getFile(filename, distPath)
